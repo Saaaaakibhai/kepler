a4196d23b53686802fc3da89223f0633c8ef6b5d
S. M. Mahedi Hasan <69164727+Saaaaakibhai@users.noreply.github.com>
2023-12-17 13:37:09 +0600

    mern

--- review api/controllers/courseController.js
+++ review api/controllers/courseController.js
@@ -1,15 +1,88 @@
-const Course = require("../models/course");
-//create new product => /api/v1/product/new
-exports.newCourse = async (req, res, next) => {
-  const course = await Course.create(res.body);
-  res.status(201).json({
-    success: true,
-    course,
-  });
-};
-exports.getCourses = (req, res, next) => {
+//create new review => api/v1/review
+exports.createCourseReview = catchAsyncErrors(async (req, res, next) => {
+  const { rating, comment, CourseId } = req.body;
+  const review = {
+    user: req.user._id,
+    name: req.user.name,
+    rating: Number(rating),
+    comment,
+  };
+  const course = await course.findById(CourseId);
+  // console.log(course.reviews);
+
+  const isReviewed = course.review.find(
+    (r) => r.user.toString() === req.user._id.toString()
+  );
+  if (isReviewed) {
+    product.reviews.foreach((review) => {
+      if (review.user.toString() === req.user._id.toString()) {
+        review.comment = comment;
+        review.rating = rating;
+      }
+    });
+  } else {
+    course.reviews.push(review);
+    course.numofReviews = product.reviews.length;
+  }
+  course.ratings =
+    course.reviews.reduce((acc, item) => item.rating + acc, 0) /
+    course.reviews.length;
+  await course.save({ validateBeforeSave: false });
   res.status(200).json({
     success: true,
-    message: "This route will show all courses in database.",
   });
-};
+});
+
+//get all courses reviews => /api1/v1/reviews
+exports.getCourseReviews = catchAsyncErrors(async (req, res, next) => {
+  const course = await Course.findById(req.query.id);
+  res.status(200).json({
+    success: true,
+    reviews: course.reviews,
+  });
+});
+
+//delete courses review => /api1/v1/reviews
+exports.deleteReview = catchAsyncErrors(async (req, res, next) => {
+  const course = await Course.findById(req.query.CourseId);
+  const reviews = course.reviews.filter(
+    (review) => review._id.toString() !== req.query.id.toString()
+  );
+  const numofReviews = reviews.length;
+  course.ratings =
+    course.reviews.reduce((acc, item) => item.rating + acc, 0) /
+    course.reviews.length;
+  await Course.findByIdAndUpdate(
+    req.query.id,
+    {
+      reviews,
+      rating,
+      numofReviews,
+    },
+    {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    }
+  );
+  res.status(200).json({
+    success: true,
+    //reviews: course.reviews,
+  });
+});
+
+// const Course = require("../models/course");
+// //create new course => /api/v1/course/new
+// exports.newCourse = async (req, res, next) => {
+//   const course = await Course.create(res.body);
+//   res.status(201).json({
+//     success: true,
+//     course,
+//   });
+// };
+// exports.getCourses = (req, res, next) => {
+//   res.status(200).json({
+//     success: true,
+//     message: "This route will show all courses in database.",
+//   });
+// };
--- review api/routes/course.js
+++ review api/routes/course.js
@@ -1,19 +1,38 @@
 const express = require("express");
 const router = express.Router();
 
+// const {
+//   getCourses,
+//   newProduct,
+//   newCourse,
+// } = require("../controllers/courseController");
+
 const {
   getCourses,
-  newProduct,
   newCourse,
+  getSingleCourse,
+  updateCourse,
+  deleteCourse,
+  createCourseReview,
+  getCourseReviews,
+  deleteReview,
 } = require("../controllers/courseController");
-
-// const {
-//     createCourseReview,
-// }
+const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
 
 router.route("/courses").get(getCourses);
-router.route("/course/new").post(newCourse);
+router.route("/course/:id").get(getSingleCourse);
 
-router.route("/review").put(isAuthenticatedUser, createProductReview);
+// router.route("/course/new").post(newCourse);
+router
+  .route("admin/course/new")
+  .post(isAuthenticatedUser, authorizeRoles("admin"), newProduct);
+router
+  .route("/admin/course/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateCourse)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteCourse);
+
+router.route("/review").put(isAuthenticatedUser, createCourseReview);
+router.route("/review").get(isAuthenticatedUser, getCourseReviews);
+router.route("/review").delete(isAuthenticatedUser, deleteReview);
 
 module.exports = router;
